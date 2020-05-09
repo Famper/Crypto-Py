b5f88df7e615aa0dddd4729949f8545cdfc50b15
Paul <posipov23@gmail.com>
2020-05-09 22:30:31 +0300

    Add Encode

--- crypto.py
+++ crypto.py
@@ -4,18 +4,5 @@ import main
 
 Text = ''
 
-def Check_Argv():
-    res = ''
-    all = 0
-    i = (-1)
-    for let in sys.argv: i += 1
-    while (i > 0):
-        res += sys.argv[i]
-        all += len(sys.argv[i])
-        i -= 1
-    print('Result: {r}'.format(r = all))
-
-    return res
-
-Text = Check_Argv()
+Text = main.Check_Argv()
 main.Crypto_Encode(Text)
--- modules/main.py
+++ modules/main.py
@@ -1,3 +1,39 @@
+import sys, random, string
+
 def Crypto_Encode(text):
+    res = ["rot1", "rot13", "rot24"]
+    rot1 = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        "BCDEFGHIJKLMNOPQRSTUVWXYZAbcdefghijklmnopqrstuvwxyza1234567890"
+    )
+    rot13 = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm3456789012"
+    )
+    rot24 = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        "YZABCDEFGHIJKLMNOPQRSTUVWXyzabcdefghijklmnopqrstuvwx4567890123"
+    )
+    enc = random.randint(0, 2)
+    print("Crypto: {r}".format(r = res[enc]))
+    if (enc == 0):
+        print("Result: {r}\n".format(r = str.translate(text, rot1)))
+        return (str.translate(text, rot1))
+    elif (enc == 1):
+        print("Result: {r}\n".format(r = str.translate(text, rot13)))
+        return (str.translate(text, rot13))
+    elif (enc == 2):
+        print("Result: {r}\n".format(r = str.translate(text, rot24)))
+        return (str.translate(text, rot24))
+    return text
+
+def Check_Argv():
     res = ''
+    i = (-1)
+    for let in sys.argv: i += 1
+    while (i > 0):
+        res += sys.argv[i]
+        i -= 1
+    print('Result: {r}'.format(r = len(res)))
+
     return res
