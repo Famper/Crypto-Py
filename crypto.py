94c26c4aafd9e50f4e1109c453b2bcd002b18bd2
Paul <posipov23@gmail.com>
2020-05-11 13:26:22 +0300

    Fix code

--- crypto.py
+++ crypto.py
@@ -3,6 +3,9 @@ import main
 
 type, check = main.Check_Argv(sys.argv[1], sys.argv[2], int(sys.argv[3]))
 
+if (check is False):
+    print('Error: Non-Argument ')
+
 if (type == 'enc'):
     main.Crypto_Encode(sys.argv[1], int(sys.argv[3]))
 else:
